9cef6faa4a972dd2d830a6bcb2a3b1ded0799a2e
Infarh <shmachilin@yandex.ru>
2020-02-24 19:38:13 +0300

    Базовая настройка контроллеров WebAPI Identity

---
+++ Services/WebStore.ServiceHosting/Controllers/RolesApiController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DAL.Context;
+using WebStore.Domain;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route(WebAPI.Identity.Roles)]
+    [Produces("application/json")]
+    [ApiController]
+    public class RolesApiController : ControllerBase
+    {
+        private readonly RoleStore<Role, WebStoreContext> _RoleStore;
+
+        public RolesApiController(WebStoreContext db) => _RoleStore = new RoleStore<Role, WebStoreContext>(db);
+    }
+}
---
+++ Services/WebStore.ServiceHosting/Controllers/UsersApiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DAL.Context;
+using WebStore.Domain;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route(WebAPI.Identity.Users)]
+    [Produces("application/json")]
+    [ApiController]
+    public class UsersApiController : ControllerBase
+    {
+        private readonly UserStore<User, Role, WebStoreContext> _UserStore;
+
+        public UsersApiController(WebStoreContext db) => _UserStore = new UserStore<User, Role, WebStoreContext>(db);
+    }
+}
