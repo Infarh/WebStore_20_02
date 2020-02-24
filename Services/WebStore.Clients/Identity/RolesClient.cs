d89991b58b40ea1449ae24c64424b413f8c57d69
Infarh <shmachilin@yandex.ru>
2020-02-24 20:09:18 +0300

    Клиенты WebAPI системы Identity

---
+++ Services/WebStore.Clients/Identity/RolesClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
+using WebStore.Domain;
+
+namespace WebStore.Clients.Identity
+{
+    public class RolesClient : BaseClient
+    {
+        public RolesClient(IConfiguration config) : base(config, WebAPI.Identity.Roles) { }
+
+    }
+}
---
+++ Services/WebStore.Clients/Identity/UsersClient.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
+using WebStore.Domain;
+
+namespace WebStore.Clients.Identity
+{
+    public class UsersClient : BaseClient
+    {
+        public UsersClient(IConfiguration config) : base(config, WebAPI.Identity.Users) { }
+    }
+}
