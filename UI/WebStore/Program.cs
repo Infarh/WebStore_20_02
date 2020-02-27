c3d382d97504b434d34f94a947a1af0faea11348
Infarh <shmachilin@yandex.ru>
2020-02-27 19:56:27 +0300

    Конфигурирование системы логирования в коде

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Options;
 
 namespace WebStore
 {
@@ -12,6 +15,13 @@ namespace WebStore
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+               //.ConfigureLogging((host, log) =>
+               // {
+               //     log.AddFilter("Microsoft", level => level > LogLevel.Information);
+               //     log.ClearProviders();
+               //     log.AddConsole(opt => opt.IncludeScopes = true);
+               //     log.AddDebug();
+               // })
                 //.UseUrls("http://0.0.0.0:8080")
                 .UseStartup<Startup>();
     }
