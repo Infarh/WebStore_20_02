59f318f04a87348341febe90c0d972689dc3ea1f
Infarh <shmachilin@yandex.ru>
2020-02-27 21:19:55 +0300

    Промежуточное ПО перехвата исключений готово

--- UI/WebStore/Infrastructure/Middleware/ErrorHandlingMiddleware.cs
+++ UI/WebStore/Infrastructure/Middleware/ErrorHandlingMiddleware.cs
@@ -1,21 +1,37 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace WebStore.Infrastructure.Middleware
 {
     public class ErrorHandlingMiddleware
     {
         private readonly RequestDelegate _Next;
+        private readonly ILogger<ErrorHandlingMiddleware> _Logger;
 
-        public ErrorHandlingMiddleware(RequestDelegate Next)
+        public ErrorHandlingMiddleware(RequestDelegate Next, ILogger<ErrorHandlingMiddleware> Logger)
         {
             _Next = Next;
+            _Logger = Logger;
         }
 
         public async Task Invoke(HttpContext Context)
         {
-            await _Next(Context);
+            try
+            {
+                await _Next(Context);
+            }
+            catch (Exception e)
+            {
+                HandleException(Context, e);
+                throw;
+            }
+        }
+
+        private void HandleException(HttpContext Context, Exception Error)
+        {
+            _Logger.LogError(Error, "Ошибка при обработке запроса {0}", Context.Request.Path);
         }
     }
 }
