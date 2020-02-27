48a45cc163653afb8eae09363cecbc3dbb1d6798
Infarh <shmachilin@yandex.ru>
2020-02-27 20:25:38 +0300

    Метод-расширения для добавления провайдера логирования log4net

---
+++ Common/WebStore.Logger/Log4NetProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace WebStore.Logger
+{
+    public class Log4NetProvider : ILoggerProvider
+    {
+        public Log4NetProvider(string ConfigurationFile) { }
+
+        public ILogger CreateLogger(string categoryName) { throw new NotImplementedException(); }
+
+        public void Dispose() { throw new NotImplementedException(); }
+    }
+}
