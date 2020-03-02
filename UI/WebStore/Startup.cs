b06b40220220acbcb845d4e4f23928a66ee8d8bb
Infarh <shmachilin@yandex.ru>
2020-03-02 21:14:12 +0300

    Преобразвоание сервиса корзины

--- UI/WebStore/Startup.cs
+++ UI/WebStore/Startup.cs
@@ -37,7 +37,7 @@ namespace WebStore
 
             services.AddSingleton<IEmployeesData, EmployeesClient>();
             services.AddScoped<IProductData, ProductsClient>();
-            services.AddScoped<ICartService, CookieCartService>();
+            services.AddScoped<ICartService, CartService>();
             services.AddScoped<ICartStore, CookiesCartStore>();
             services.AddScoped<IOrderService, OrdersClient>();
 
