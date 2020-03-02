d7847a7d8b1b3b521fcd9e0df18768dac78c71ee
Infarh <shmachilin@yandex.ru>
2020-03-02 20:05:09 +0300

    HomeController - модульные тесты

--- UI/WebStore/Controllers/HomeController.cs
+++ UI/WebStore/Controllers/HomeController.cs
@@ -22,5 +22,15 @@ namespace WebStore.Controllers
         public IActionResult BlogSingle() => View();
         public IActionResult ContactUs() => View();
         public IActionResult Error404() => View();
+
+        public IActionResult ErrorStatus(string Id)
+        {
+            switch (Id)
+            {
+                default: return Content($"Статусный код {Id}");
+                case "404":
+                    return RedirectToAction(nameof(Error404));
+            }
+        }
     }
 }
