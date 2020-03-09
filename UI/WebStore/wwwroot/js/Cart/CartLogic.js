a965d52ee9e096dd02715c1a76aefca57f4e3438
Infarh <shmachilin@yandex.ru>
2020-03-09 20:24:38 +0300

    Асинхронное добавление товаров в корзину

---
+++ UI/WebStore/wwwroot/js/Cart/CartLogic.js
@@ -0,0 +1,47 @@
+﻿Cart = {
+    _properties: {
+        getCartViewLink: "",
+        addToCartLink: ""
+    },
+
+    init: function(properties) {
+        $.extend(Cart._properties, properties);
+
+        Cart.initEvents();
+    },
+
+    initEvents: function() {
+        $(".add-to-cart").click(Cart.addToCart);
+    },
+
+    addToCart: function(event) {
+        event.preventDefault();
+
+        var button = $(this);
+        const id = button.data("id"); //data-id="..."
+
+        //$.get(Cart._properties.addToCartLink + "/" + id)
+        $.get(`${Cart._properties.addToCartLink}/${id}`)
+            .done(function() {
+                Cart.showToolTip(button);
+                Cart.refreshCartView();
+            })
+            .fail(function () { console.log("addToCart file"); });
+    },
+
+    showToolTip: function(button) {
+        button.tooltip({ title: "Добавлено в корзину" }).tooltip("show");
+        setTimeout(function() {
+            button.tooltip("destroy");
+        },500);
+    },
+
+    refreshCartView: function() {
+        var container = $("#cart-container");
+        $.get(Cart._properties.getCartViewLink)
+            .done(function(cartHtml) {
+                container.html(cartHtml);
+            })
+            .fail(function () { console.log("refreshCartView fail"); });
+    }
+}
